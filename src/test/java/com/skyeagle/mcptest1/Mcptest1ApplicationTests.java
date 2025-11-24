169f0b536ecf2b572a5389ed6c53428d5b17b65d
Ramdas Bhagat <ramdas.bhagat222@gmail.com>
2025-11-24 11:14:02 +0530

    Initial project structure

---
+++ src/test/java/com/skyeagle/mcptest1/Mcptest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyeagle.mcptest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcptest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
